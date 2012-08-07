ee4c21cb8b85671ea1575b9ca441de023698f9b4
Panu Leppäniemi <me@panuleppaniemi.com>
2012-08-07 22:48:13 +0300

    Load index.md by default.

--- src/app.php
+++ src/app.php
@@ -2,19 +2,23 @@
 
 $app = require_once __DIR__.'/bootstrap.php';
 
-$app->get('/', function() {
-    return '?';
+$app->get('/', function() use($app) {
+    return render('index', $app);
+});
+
+$app->get('/{topic}/', function($topic) use($app) {
+    return render($topic, $app);
 });
 
 use dflydev\markdown\MarkdownParser;
 
-$app->get('/{topic}/', function($topic) use($app) {
+function render($filename, $app) {
     $parser  = new MarkdownParser();
-    $content = @file_get_contents(__DIR__.'/../data/'. $topic . '.md'); // TODO: Security.
+    $content = @file_get_contents(__DIR__.'/../data/'.$filename.'.md'); // TODO: Security.
 
     return $app['twig']->render('view.html.twig', array(
         'story' => $parser->transform($content ?: "#404")
     ));
-});
+}
 
 return $app;
